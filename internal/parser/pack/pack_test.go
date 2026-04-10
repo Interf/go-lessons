cd246ecbf69c1465a0d8a7121ef4a7a3537a27df
Артем Хозяинов <a.khozyainov@qsoft.ru>
2026-04-10 20:04:59 +0300

    tests

---
+++ internal/parser/pack/pack_test.go
@@ -0,0 +1,29 @@
+package pack
+
+import "testing"
+
+func TestPack(t *testing.T) {
+	tests := map[string]struct {
+		Text string
+		Want string
+	}{
+		"default": {
+			Text: "aaaabccddddde",
+			Want: "a4bc2d5e",
+		},
+		"empty string": {
+			Text: "",
+			Want: "",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := Pack(test.Text)
+
+			if got != test.Want {
+				t.Errorf("Test: %s. Got %s, want %s", name, got, test.Want)
+			}
+		})
+	}
+}
---
+++ internal/parser/unpack/pack_test.go
@@ -0,0 +1,112 @@
+package unpack
+
+import (
+	"testing"
+)
+
+func TestUnpack(t *testing.T) {
+
+	tests := map[string]struct {
+		Text string
+		Want string
+	}{
+		"default": {
+			Text: "a4bc2d5e",
+			Want: "aaaabccddddde",
+		},
+		"without digit": {
+			Text: "abcd",
+			Want: "abcd",
+		},
+		"first Digit": {
+			Text: "3abc",
+			Want: "",
+		},
+		"first number": {
+			Text: "45",
+			Want: "",
+		},
+		"empty string": {
+			Text: "",
+			Want: "",
+		},
+		"escape symbol": {
+			Text: "d\\n5abc",
+			Want: "d\\n\\n\\n\\n\\nabc",
+		},
+		"number": {
+			Text: "f35",
+			Want: "",
+		},
+		"zero digit": {
+			Text: "qwe0",
+			Want: "qw",
+		},
+		"raw string": {
+			Text: "`qwe\\4\\5`",
+			Want: "qwe45",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := Unpack(test.Text)
+
+			if got != test.Want {
+				t.Errorf("Test: %s. got %s, want %s", name, got, test.Want)
+			}
+
+		})
+	}
+
+}
+
+func TestUnpackRaw(t *testing.T) {
+	tests := map[string]struct {
+		Text string
+		Want string
+	}{
+		"default": {
+			Text: "`qwe\\4\\5`",
+			Want: "qwe45",
+		},
+		"escape number": {
+			Text: "`qwe\\45`",
+			Want: "qwe44444",
+		},
+		"escape slash": {
+			Text: "`qwe\\\\5`",
+			Want: "qwe\\\\\\\\\\",
+		},
+		"escape without digit": {
+			Text: "`qw\\\ne`",
+			Want: "",
+		},
+		"empty string": {
+			Text: "",
+			Want: "",
+		},
+		"first Digit": {
+			Text: "`3abc`",
+			Want: "",
+		},
+		"number": {
+			Text: "`f35`",
+			Want: "",
+		},
+		"zero digit": {
+			Text: "`qwe0`",
+			Want: "qw",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, _ := unpackRaw(test.Text)
+
+			if got != test.Want {
+				t.Errorf("Test: %s. got %s, want %s", name, got, test.Want)
+			}
+		})
+	}
+}
